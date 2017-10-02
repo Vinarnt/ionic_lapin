66f73d8fa84a29301e57e562ab7bd111c3ed767e
Vinarnt <contact@vinarnt.fr>
2017-10-02 09:22:36 +0200

    Remove unused dependency

--- www/app/domain/services/domain.js
+++ www/app/domain/services/domain.js
@@ -1,28 +1,12 @@
 angular.module('starter.services')
-  .factory("Domain", function($http, CacheFactory, Api) {
+    .factory("Domain", function ($http, Api) {
 
-    var domainCache;
-
-    if (!CacheFactory.get('domainCache')) {
-
-      domainCache = CacheFactory('domainCache', {
-        maxAge: 24 * 60 * 60 * 1000,
-        deleteOnExpire: 'aggressive',
-        storageMode: 'localStorage'
-      });
-
-    }
-
-    return {
-      returnDomain: function() {
-        return $http.get(`${Api.baseUrl}infoGeneral`, {
-          cache: domainCache
-        });
-      },
-      returnInfo: function(domain) {
-        return $http.get(`${Api.baseUrl}info/${domain}`, {
-          cache: domainCache
-        });
-      }
-    };
-  });
+        return {
+            returnDomain: function () {
+                return $http.get(`${Api.baseUrl}infoGeneral`);
+            },
+            returnInfo: function (domain) {
+                return $http.get(`${Api.baseUrl}info/${domain}`);
+            }
+        };
+    });
